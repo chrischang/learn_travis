17719bdfaeda8e5d8f4be9e529e3e2b4625edb4f
Chris Chang <chrischang43@gmail.com>
2016-07-29 14:28:00 -0700

    testing simple calculator

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-function plus(a,b) {
+function add(a,b) {
 	return a + b;
 }
 
+function subtract(a,b) {
+	return a - b;
+}
+
+function multiplication(a,b) {
+	return a * b;
+}
+
+function division(a,b) {
+	return a / b;
+}
+
+function calculator (a, b, cb) {
+	return cb(a,b);
+}
+
 module.exports = {
-	plus
+	add,
+	subtract,
+	multiplication,
+	division,
+	calculator
 };
--- test/test.js
+++ test/test.js
@@ -2,10 +2,16 @@ var code = require('../main');
 var expect = require('chai').expect;
 
 describe('math_problem', function() {
-	it('should return 3', function() {
-		expect(code.plus(1, 2)).to.be.equal(3);
+	it('3 + 3 should return 6', function() {
+		expect(code.calculator(3,3,code.add)).to.be.equal(6);
 	})
-	it('should return 5', function() {
-		expect(code.plus(2, 3)).to.be.equal(5);
+	it('3 - 3 should return 0', function() {
+		expect(code.calculator(3,3,code.subtract)).to.be.equal(0);
+	})
+	it('3 * 3 should return 9', function() {
+		expect(code.calculator(3,3,code.multiplication)).to.be.equal(9);
+	})
+	it('3 / 3 should return 1', function() {
+		expect(code.calculator(3,3,code.division)).to.be.equal(1);
 	})
 })
